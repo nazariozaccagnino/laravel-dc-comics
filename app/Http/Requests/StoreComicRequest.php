2625097558a61ae4a9279b1f1c1f68d9d8eb8437
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-05-29 17:02:44 +0200

    created custom messages

--- app/Http/Requests/StoreComicRequest.php
+++ app/Http/Requests/StoreComicRequest.php
@@ -24,13 +24,32 @@ class StoreComicRequest extends FormRequest
     public function rules()
     {
         return [
-            'title'=>'required|max:255|min:1',
-            'description'=>'required|max:255|min:1',
-            'thumb'=>'required|max:255|min:1',
-            'price'=>'required|numeric|integer|max_digits:10|min:1',
-            'series'=>'required|max:255|min:1',
-            'sale_date'=>'required|date',
-            'type'=>'required|max:255|min:1',            
+            'title' => 'required|max:255|min:1',
+            'description' => 'required|max:255|min:1',
+            'thumb' => 'required|max:255|min:1',
+            'price' => 'required|numeric|integer|max_digits:10|min:1',
+            'series' => 'required|max:255|min:1',
+            'sale_date' => 'required|date',
+            'type' => 'required|max:255|min:1',
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'E\' necessario inserire un titolo',
+            'title.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
+            'description.required' => 'E\' necessario inserire una descrizione',
+            'description.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
+            'thumb.required' => 'E\' necessario inserire un link ad una immagine',
+            'thumb.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
+            'price.required' => 'E\' necessario inserire un costo',
+            'price.numeric' => 'Inserire un valore numerico',
+            'series.required' => 'E\' necessario inserire un nome per la serie',
+            'series.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
+            'sale_date.required' => 'E\' necessario inserire una data di inizio vendite',
+            'sale_date.date' => 'Inserisci una data in formato DD/MM/YYYY',
+            'type.required' => 'E\' necessario inserire una tipologia per il fumetto',
+            'type.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
         ];
     }
 }
--- app/Http/Requests/UpdateComicRequest.php
+++ app/Http/Requests/UpdateComicRequest.php
@@ -33,4 +33,23 @@ class UpdateComicRequest extends FormRequest
             'type'=>'required|max:255|min:1',
         ];
     }
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'E\' necessario inserire un titolo',
+            'title.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
+            'description.required' => 'E\' necessario inserire una descrizione',
+            'description.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
+            'thumb.required' => 'E\' necessario inserire un link ad una immagine',
+            'thumb.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
+            'price.required' => 'E\' necessario inserire un costo',
+            'price.numeric' => 'Inserire un valore numerico',
+            'series.required' => 'E\' necessario inserire un nome per la serie',
+            'series.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
+            'sale_date.required' => 'E\' necessario inserire una data di inizio vendite',
+            'sale_date.date' => 'Inserisci una data in formato DD/MM/YYYY',
+            'type.required' => 'E\' necessario inserire una tipologia per il fumetto',
+            'type.max' => 'Hai inserito troppi caratteri, riduci la dimensione del testo',
+        ];
+    }
 }
