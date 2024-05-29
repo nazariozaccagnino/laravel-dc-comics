e04f4bdd5e7d1aa32f7862715e888e902d77ca4b
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-05-29 17:16:06 +0200

    palced in pages the errors below the forms

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,45 +5,57 @@
 @section('content')
 <section>
   <div class="container">
-    @if ($errors->any())
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{$error}}</li>
-        @endforeach
-      </ul>
-    </div>
-    @endif
     <form action="{{route('comics.store')}}" method="POST">
       @csrf
       <div class="mb-3">
         <label for="title" class="form-label">Title</label>
         <input type="text" class="form-control" id="title" name="title" placeholder="Insert a title">
       </div>
+      @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="description" class="form-label">Description</label>
         <input type="text" class="form-control" id="description" name="description" placeholder="Insert a description">
       </div>
+      @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="thumb" class="form-label">Image link</label>
         <input type="text" class="form-control" id="thumb" name="thumb" placeholder="Insert an image link">
       </div>
+      @error('thumb')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="price" class="form-label">Price</label>
         <input type="number" class="form-control" id="price" name="price" placeholder="Insert item price">
       </div>
+      @error('price')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="series" class="form-label">Series</label>
         <input type="text" class="form-control" id="series" name="series" placeholder="Insert the series name">
       </div>
+      @error('series')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="sale_date" class="form-label">Sale date</label>
         <input type="date" class="form-control" id="sale_date" name="sale_date">
       </div>
+      @error('sale_date')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
-        <label for="tyoe" class="form-label">Type</label>
+        <label for="type" class="form-label">Type</label>
         <input type="text" class="form-control" id="type" name="type" placeholder="Insert the item type">
       </div>
+      @error('type')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
   </div>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,15 +5,6 @@
 @section('content')
 <section>
   <div class="container">
-  @if ($errors->any())
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{$error}}</li>
-        @endforeach
-      </ul>
-    </div>
-    @endif
     <form action="{{route('comics.update', $comic->id)}}" method="POST">
       @csrf
       @method('PUT')
@@ -21,30 +12,51 @@
         <label for="title" class="form-label">Title</label>
         <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}">
       </div>
+      @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="description" class="form-label">Description</label>
         <input type="text" class="form-control" id="description" name="description" value="{{$comic->description}}">
       </div>
+      @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="thumb" class="form-label">Image link</label>
         <input type="text" class="form-control" id="thumb" name="thumb" value="{{$comic->thumb}}">
       </div>
+      @error('thumb')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="price" class="form-label">Price</label>
         <input type="number" class="form-control" id="price" name="price" value="{{$comic->price}}">
       </div>
+      @error('price')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="series" class="form-label">Series</label>
         <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
       </div>
+      @error('series')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="sale_date" class="form-label">Sale date</label>
         <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
       </div>
+      @error('sale_date')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="mb-3">
         <label for="type" class="form-label">Type</label>
         <input type="text" class="form-control" id="type" name="type" value="{{$comic->type}}">
       </div>
+      @error('type')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
       <div class="py-2">
       <button type="submit" class="btn btn-primary">Modify</button>
       <a href="{{route('comics.index')}}" class="btn btn-secondary">Return</a>
